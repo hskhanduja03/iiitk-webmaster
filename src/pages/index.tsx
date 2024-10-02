f5317768312d81e05f9e8c9b5acf8ef9a3bdf1f7
Harmeet Singh <hskhanduja03@gmail.com>
2024-10-03 03:47:50 +0530

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,145 @@
+import { useEffect, useState } from "react";
+import { gsap } from "gsap";
 import Image from "next/image";
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [currentValue, setCurrentValue] = useState(0); // Step 1: State for counter value
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    const startLoader = () => {
+      const counterElement =
+        document.querySelector<HTMLHeadingElement>(".counter");
+      let currentValue = 0;
+
+      const updateCounter = () => {
+        if (currentValue === 100) return;
+        currentValue += Math.floor(Math.random() * 10) + 1;
+
+        if (currentValue > 100) currentValue = 100;
+        if (counterElement) {
+          counterElement.textContent = currentValue.toString();
+        }
+        setCurrentValue(Math.min(currentValue, 100));
+        const delay = Math.floor(Math.random() * 200) + 50;
+        setTimeout(updateCounter, delay);
+      };
+
+      updateCounter();
+    };
+
+    startLoader();
+
+    gsap.to(".counter", {
+      opacity: 0,
+      delay: 3.0,
+      duration: 0.25,
+    });
+
+    gsap.to(".bar", {
+      height: 0,
+      delay: 3.5,
+      stagger: {
+        amount: 0.5,
+      },
+      ease: "power4.inOut",
+      duration: 1.5,
+    });
+
+    // Animate the IIIT image
+    gsap.from(
+      ".iiit",
+      {
+        delay:3.5,
+        scale: 2, // Start with a larger size
+        x: "40vw", // Start at the center
+        y: "40vh",
+        transformOrigin: "center center", // Set the origin for scaling
+      }
+    );
+
+    gsap.from(".h1", {
+      y: 800,
+      delay: 4,
+      stagger: {
+        amount: 0.7,
+      },
+      ease: "power4.inOut",
+      duration: 1.5,
+    });
+
+    gsap.from(".hero", {
+      y: 800,
+      opacity: 0,
+      delay: 4,
+      ease: "power4.inOut",
+      duration: 2,
+    });
+  }, []);
+
+  return (
+    <div className="relative w-full h-screen bg-gray-300 overflow-hidden">
+      <h1 className="lg:text-[10vw] counter text-white absolute flex justify-end items-end w-full h-full text-[15vw] p-2 font-carpenter z-30">
+        {currentValue + "%"}
+      </h1>
+
+      <div className=" lg:text-[10vw] overlay absolute flex w-screen h-screen">
+        {[...Array(8)].map((_, idx) => (
+          <div key={idx} className="bar w-[20vw] h-[105vh] bg-gray-900"></div>
+        ))}
+      </div>
+
+      <div className="header flex justify-between p-4 w-full items-center">
+        <div className="iiit relative w-[10vw] h-[20vh] mx-auto rounded-lg mb-5">
+          <Image
+            src="/iiit.png"
+            alt="Panda"
+            layout="fill"
+            objectFit="contain"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="flex h1 lg:text-[10vw] sm:text-[10vw] md:text-[10vw] text-[10vw] font-bold font-carpenter leading-tight">
+          <h1>I</h1>
+          <h1>I</h1>
+          <h1>I</h1>
+          <h1>T</h1>
+        </div>
+        <div className="flex h1 lg:text-[10vw] sm:text-[10vw] md:text-[10vw] text-[10vw] font-bold font-carpenter leading-tight">
+          <h1>K</h1>
+          <h1>O</h1>
+          <h1>T</h1>
+          <h1>A</h1>
+        </div>
+        <div className="h1 lg:text-[10vw] sm:text-[10vw] md:text-[10vw] text-[10vw] font-bold font-carpenter leading-tight">
+          -
+        </div>
+        <div className="h1 lg:text-[10vw] sm:text-[10vw] md:text-[10vw] text-[10vw] font-bold font-carpenter leading-tight">
+          E
+        </div>
+        <div className="h1 lg:text-[10vw] sm:text-[10vw] md:text-[10vw] text-[10vw] font-bold font-carpenter leading-tight">
+          V
+        </div>
+        <div className="h1 lg:text-[10vw] sm:text-[10vw] md:text-[10vw] text-[10vw] font-bold font-carpenter leading-tight">
+          E
+        </div>
+        <div className="h1 lg:text-[10vw] sm:text-[10vw] md:text-[10vw] text-[10vw] font-bold font-carpenter leading-tight">
+          N
+        </div>
+        <div className="h1 lg:text-[10vw] sm:text-[10vw] md:text-[10vw] text-[10vw] font-bold font-carpenter leading-tight">
+          T
+        </div>
+        <div className="h1 lg:text-[10vw] sm:text-[10vw] md:text-[10vw] text-[10vw] font-bold font-carpenter leading-tight">
+          S
+        </div>
+      </div>
+
+      <div className="hero relative w-[calc(70vw-4em)] h-[70vh] mx-auto rounded-lg border-4 border-black">
+        <Image
+          src="https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cG9ydHJhaXR8ZW58MHx8MHx8fDA%3D"
+          alt="Panda"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        carpenter: ['Carpenter', 'sans-serif'],
+      },
     },
   },
   plugins: [],
