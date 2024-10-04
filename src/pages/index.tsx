74bf83ce45428b569a3db6240fe98a1af6255305
Harmeet Singh <hskhanduja03@gmail.com>
2024-10-04 20:59:51 +0530

    minor carousel changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,6 @@ import { gsap } from "gsap";
 import Image from "next/image";
 
 export default function Home() {
-  const slides = [
-    "https://i.ibb.co/ncrXc2V/1.png",
-    "https://i.ibb.co/B3s7v4h/2.png",
-    "https://i.ibb.co/XXR8kzF/3.png",
-    "https://i.ibb.co/yg7BSdM/4.png",
-  ];
   const [currentValue, setCurrentValue] = useState(0);
 
   useEffect(() => {
